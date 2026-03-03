ff11b9d2a68c7f46f284003572b7e42a464502a0
Richard Leyshon <richard.leyshon@justice.gov.uk>
2026-03-03 08:50:13 +0000

    fix: project vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -11,7 +12,7 @@ const nextConfig: NextConfig = {
     ],
   },
   turbopack: {
-    root: process.cwd(),
+    root: path.join(process.cwd()),
   },
 };
 
